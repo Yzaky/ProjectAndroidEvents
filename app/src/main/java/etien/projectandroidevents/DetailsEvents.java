293444facd54f0c6cba4e57d554960fcea698289
Etienne Monette <etiennem1553@gmail.com>
2017-03-06 17:59:01 -0500

    listview et événements détaillés fonctionnels

--- app/src/main/java/etien/projectandroidevents/DetailsEvents.java
+++ app/src/main/java/etien/projectandroidevents/DetailsEvents.java
@@ -2,6 +2,7 @@ package etien.projectandroidevents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DetailsEvents extends AppCompatActivity {
 
@@ -9,5 +10,33 @@ public class DetailsEvents extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_events);
+
+        String title, starttime, description;
+        if (savedInstanceState == null) {
+            Bundle extras = getIntent().getExtras();
+            if(extras == null) {
+                title= null;
+                starttime = null;
+                description = null;
+            } else {
+                title= extras.getString("Title");
+                starttime = extras.getString("StartTime");
+                description = extras.getString("Description");
+            }
+        } else {
+            title= (String) savedInstanceState.getSerializable("Titles");
+            starttime= (String) savedInstanceState.getSerializable("StartTime");
+            description= (String) savedInstanceState.getSerializable("Description");
+        }
+
+        TextView textViewTitle = (TextView) findViewById(R.id.textViewTitle);
+        textViewTitle.setText(title);
+
+        TextView textViewStarttime = (TextView) findViewById(R.id.textViewStarttime);
+        textViewStarttime.setText(starttime);
+
+        TextView textViewDescription = (TextView) findViewById(R.id.textViewDescription);
+        textViewDescription.setText(description);
+
     }
 }
--- app/src/main/java/etien/projectandroidevents/ListEvents.java
+++ app/src/main/java/etien/projectandroidevents/ListEvents.java
@@ -1,13 +1,118 @@
 package etien.projectandroidevents;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class ListEvents extends AppCompatActivity {
+
+    ListView liste;
+
+    int page_size = 10;
+    private String[] titles = new String[page_size];
+    private String[] descriptions = new String[page_size];
+    private String[] start_times = new String[page_size];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_events);
+
+        FetchArticle fetcher = new FetchArticle();
+        fetcher.execute();
+        liste = (ListView) findViewById(R.id.list);
+
+        liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), DetailsEvents.class);
+
+                intent.putExtra("Title",titles[position]);
+                intent.putExtra("StartTime",start_times[position]);
+                intent.putExtra("Description",descriptions[position]);
+
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    public class SimpleListAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return page_size;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return 0;
+        }
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            if(convertView == null) {
+                convertView = getLayoutInflater().inflate(android.R.layout.simple_list_item_2, parent, false);
+            }
+
+            TextView tv = (TextView) convertView.findViewById(android.R.id.text1);
+            TextView tv2 = (TextView) convertView.findViewById(android.R.id.text2);
+
+            tv.setText(titles[position % page_size]);
+            tv2.setText(start_times[position % page_size]);
+
+            return convertView;
+        }
+    }
+
+    public class FetchArticle extends AsyncTask<String, Object, OneEvent[]> {
+
+        @Override
+        protected OneEvent[] doInBackground(String... params) {
+
+            // Fetcher un event
+
+            EventfulAPI api = new EventfulAPI();
+
+            OneEvent[] events = new OneEvent[page_size];
+
+            try {
+                events = api.getAllEvents();
+
+            } catch (IOException |JSONException e) {
+                e.printStackTrace();
+            }
+
+            return events;
+        }
+
+        @Override
+        protected void onPostExecute(OneEvent[] events) {
+
+            for(int i=0; i<page_size; i++) {
+                titles[i] = events[i].title;
+                descriptions[i] = events[i].description;
+                start_times[i] = events[i].start_time;
+            }
+
+            liste.setAdapter(new SimpleListAdapter());
+        }
     }
 }
