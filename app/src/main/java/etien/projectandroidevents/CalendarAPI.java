8932412ed84abc46f29384f1919bd35c2d3f3e67
Aata-allah <atala.rchidi@gmail.com>
2017-03-23 15:08:00 -0400

    Test premier push

--- app/src/main/java/etien/projectandroidevents/CalendarAPI.java
+++ app/src/main/java/etien/projectandroidevents/CalendarAPI.java
@@ -1,7 +1,10 @@
 package etien.projectandroidevents;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CalendarContract;
