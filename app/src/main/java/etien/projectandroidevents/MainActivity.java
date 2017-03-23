08d45dab1c34d1a01aaaafff9d795abb9f96a9bd
Francis de Ladurantaye <fdl9044@gmail.com}>
2017-03-23 10:46:56 -0400

    sup sup

--- app/src/main/java/etien/projectandroidevents/MainActivity.java
+++ app/src/main/java/etien/projectandroidevents/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+//lalala
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);  //jk
