999db1ddfb933ac31e43a7573087ea8bc7e7e282
Atala <atala.rchidi@gmail.com>
2017-03-23 15:28:05 -0400

    Merge branch 'master' of /Users/atala/AndroidStudioProjects/ProjectAndroidEvents with conflicts.

--- app/src/main/java/etien/projectandroidevents/MainActivity.java
+++ app/src/main/java/etien/projectandroidevents/MainActivity.java
@@ -1,49 +1,49 @@
-package etien.projectandroidevents;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
-import android.widget.Button;
-
-public class MainActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);  //jk
-        setContentView(R.layout.activity_main);
-
-        Button buttonListEvents = (Button) findViewById(R.id.buttonListEvents);
-        buttonListEvents.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ListEvents.class);
-                startActivity(intent);
-            }
-        });
-        Button buttonCalendar = (Button) findViewById(R.id.buttonCalendar);
-        buttonCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), CalendarAPI.class);
-                startActivity(intent);
-            }
-        });
-        Button buttonGeolocalisation = (Button) findViewById(R.id.buttonGeolocalisation);
-        buttonGeolocalisation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Geolocalisation.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.my_options_menu, menu);
-        return true;
-    }
-}
+//package etien.projectandroidevents;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.view.Menu;
+//import android.view.MenuInflater;
+//import android.view.View;
+//import android.widget.Button;
+//
+//public class MainActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);  //jk
+//        setContentView(R.layout.activity_main);
+//
+//        Button buttonListEvents = (Button) findViewById(R.id.buttonListEvents);
+//        buttonListEvents.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), ListEvents.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Button buttonCalendar = (Button) findViewById(R.id.buttonCalendar);
+//        buttonCalendar.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), CalendarAPI.class);
+//                startActivity(intent);
+//            }
+//        });
+//        Button buttonGeolocalisation = (Button) findViewById(R.id.buttonGeolocalisation);
+//        buttonGeolocalisation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), Geolocalisation.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//    }
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.my_options_menu, menu);
+//        return true;
+//    }
+//}
